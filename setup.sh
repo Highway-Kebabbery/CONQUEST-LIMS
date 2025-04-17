2c85ece3c093d9adea8bdeeaa1e6510bbf0c6fa8
Highway-Kebabbery <25822892+Highway-Kebabbery@users.noreply.github.com>
2025-04-17 17:46:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -97,7 +97,7 @@ sudo dpkg -i minikube_latest_amd64.deb && \
 rm minikube_latest_amd64.deb"
 
 check_and_prompt_install kubectl \
-"curl -LO https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl && \
+"curl -LO https://dl.k8s.io/release/\$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl && \
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && \
 rm kubectl"
 
