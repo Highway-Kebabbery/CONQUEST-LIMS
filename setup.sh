6ccc93e79c265674701fd015605e4d81b3513669
Highway-Kebabbery <25822892+Highway-Kebabbery@users.noreply.github.com>
2025-04-17 17:12:35 -0400

    Update setup.sh
    Editing setup.sh

--- setup.sh
+++ setup.sh
@@ -95,6 +95,11 @@ check_and_prompt_install minikube \
 sudo dpkg -i minikube_latest_amd64.deb && \
 rm minikube_latest_amd64.deb"
 
+check_and_prompt_install kubectl \
+"curl -LO 'https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl' && \
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && \
+rm kubectl"
+
 minikube start --driver=docker
 eval $(minikube docker-env)
 docker build -t conquest-lims-api .
